dc075e736e10034fd03e61398b59c243048f1f58
Drolfothesgnir <kyryl.yeletsky@gmail.com>
2025-01-14 02:10:49 +0200

    removed redundant files; added some windows config

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 use diary_core::{db::SortOrder, Config, DiaryDB, Entry, Pagination};
 use tauri::Manager;
 use tokio::sync::mpsc;
